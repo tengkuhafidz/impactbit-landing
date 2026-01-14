d1e57836668e2496921c6aab2f927670decd3670
Hafizuddin (Hafiz) Aminuddin <hafizuddinaminuddin1@gmail.com>
2026-01-14 09:19:35 +0800

    Update: Change title to 'Monthly Update'

--- sanity/schemaTypes/monthlyUpdate.ts
+++ sanity/schemaTypes/monthlyUpdate.ts
@@ -31,7 +31,7 @@ export default defineType({
     ],
     preview: {
         select: {
-            title: 'text',
+            title: 'Monthly Update',
             media: 'thumbnail'
         }
     }
